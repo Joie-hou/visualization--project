6e31a11a78bfa97930ac0ab68dc7709d3c2ddf01
here_for_you <houjunjie@eastbabel.cn>
2023-03-28 11:12:57 +0800

    eslint prettier 配置

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,42 @@
 <template>
-  <transition>
-    <div id="app">
-      <dv-full-screen-container>
-        <GlobalHeader></GlobalHeader>
-        <router-view></router-view>
-      </dv-full-screen-container>
-      <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <transition>
+        <div id="app">
+            <dv-full-screen-container>
+                <router-view></router-view>
+            </dv-full-screen-container>
+            <!-- <img alt="Vue logo" src="./assets/logo.png">
       <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    </div>
-  </transition>
+        </div>
+    </transition>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import GlobalHeader from './components/GlobalHeader.vue'
 
 export default {
-  name: 'App',
-  components: {
-    // HelloWorld
-    GlobalHeader
-  }
-}
+    name: "App",
+    components: {
+        // HelloWorld
+    },
+};
 </script>
 
 <style>
-html, body {
-  margin: 0;
-  height: 100%;
-  width: 100%;
+html,
+body {
+    margin: 0;
+    height: 100%;
+    width: 100%;
 }
 #app {
-  height: 100%;
-  width: 100%;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  background-color: rgb(3 17 54);
-  /* margin-top: 60px; */
+    height: 100%;
+    width: 100%;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    background-color: rgb(3 17 54);
+    /* margin-top: 60px; */
 }
 </style>
