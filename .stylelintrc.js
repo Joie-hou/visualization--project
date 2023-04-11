e5df0eda9bf4b675a43f619d3685b19ca78665a9
here_for_you <houjunjie@eastbabel.cn>
2023-04-11 20:28:09 +0800

    stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -11,8 +11,15 @@ module.exports = {
             true,
             {
                 ignorePartialSupport: true,
+                browserslist: ["> 1%", "last 2 versions", "not dead"],
             },
         ],
         "selector-class-pattern": "^(([a-z][a-z0-9]*)(-[a-z0-9]+)*)|(van-[a-z0-9]*(_[a-z0-9]*)?)$",
+        // 选择器前是否有空行
+        "rule-empty-line-before": "never-multi-line",
+        // 选择器能否为空
+        "block-no-empty": null,
+        // https://stylelint.io/user-guide/rules/no-descending-specificity/#ignore-selectors-within-list
+        "no-descending-specificity": null,
     },
 };
